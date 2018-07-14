fbd92ced83de099eb6ed7ab164435f694d4bb595
daithi <webeire@gmail.com>
2018-07-14 08:03:34 +0100

    init commit

---
+++ contracts/CryptoBean.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract CryptoBean is MintableToken {
+  string public name = "CryptoBean";
+  string public symbol = "HT";
+  uint8 public decimals = 18;
+}
